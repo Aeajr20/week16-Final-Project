86513b11dfd50cf40252d7d8d6ca63cbfd3706f1
Aeajr20 <aeajr20@gmail.com>
2024-07-13 21:34:52 -0700

    updating

--- src/App.test.js
+++ src/App.test.js
@@ -1,32 +1,56 @@
-// import { render, screen } from '@testing-library/react';
-// import App from './App';
+import { render, screen } from '@testing-library/react';
+import App from './App';
 
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+
+
+// import React from 'react';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import { Container } from 'react-bootstrap';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import Home from './Pages/Home';
+// import MatchingGame from './Pages/MatchingGame'; // Assuming MatchingGame is in Pages folder
+// import './App.css';
+
+// function App() {
+//   return (
+//     <Router basename={process.env.PUBLIC_URL}>
+//       <Container>
+//         <Routes>
+//           <Route exact path="/" element={<Home />} />
+//           <Route path="/matching-game" element={<MatchingGame />} />
+//           {/* Add more routes for other pages as needed */}
+//         </Routes>
+//       </Container>
+//     </Router>
+//   );
+// }
+
+// export default App;
 
 
 
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Container } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Home from './Pages/Home';
-import MatchingGame from './Pages/MatchingGame'; // Assuming MatchingGame is in Pages folder
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home'; // Import Home component
+import MatchingGame from './Pages/MatchingGame'; // Import MatchingGame component
+import TicTacToe from './Pages/TicTacToe'; // Import TicTacToe component
 
 function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
-      <Container>
+    <Router>
+      <div className="App">
         <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/matching-game" element={<MatchingGame />} />
-          {/* Add more routes for other pages as needed */}
+          <Route path="/" exact component={Home} />
+          <Route path="/matching-game" component={MatchingGame} />
+          <Route path="/tic-tac-toe" component={TicTacToe} />
         </Routes>
-      </Container>
+      </div>
     </Router>
   );
 }
